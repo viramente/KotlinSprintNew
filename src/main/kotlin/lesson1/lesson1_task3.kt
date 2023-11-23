2b07e527d885bd702227b53666f61feec2caa516
viramente <viramente@gmail.com>
2023-11-23 14:42:20 +0200

    KS-1-3 space flight

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package lesson1
+
+fun main() {
+
+    val year = 1969
+    var hour = 9
+    var minute = 7
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+}
