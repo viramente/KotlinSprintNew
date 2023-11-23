a354fdfb3590aa26502803a4a75a6e2dd021ff31
viramente <viramente@gmail.com>
2023-11-23 02:32:18 +0200

    KS-1-1_store values naming

---
+++ src/main/kotlin/lesson1/lesson1_task.kt
@@ -0,0 +1,9 @@
+package lesson1
+
+fun main() {
+
+    println("Это решение задачи 1 из урока 1")
+    val ordersAmount: Int = 75
+    val graditudeForThePurchase: String = "We thank you for shopping in our store!"
+
+}
