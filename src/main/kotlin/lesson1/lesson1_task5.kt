a33edfdd54016dc17067f9f8069bbae1bcac5c11
viramente <viramente@gmail.com>
2023-11-24 23:05:46 +0200

    KS-1-5 Time in space... with .format

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -5,13 +5,8 @@ fun main() {
     val secondsInSpace = 6480
     val wholeHours = secondsInSpace / 3600
     val remindedWholeMinutes = (secondsInSpace - wholeHours * 3600) / 60
-    val remindedSeconds =  secondsInSpace - wholeHours*3600 - remindedWholeMinutes * 60
+    val remindedSeconds = secondsInSpace - wholeHours*3600 - remindedWholeMinutes * 60
 
-    print(0)
-    print(wholeHours)
-    print(':')
-    print(remindedWholeMinutes)
-    print(':')
-    print(remindedSeconds)
-    print(0)
+    val output = "%02d".format(wholeHours) + ':' + "%02d".format(remindedWholeMinutes) + ':' + "%02d".format(remindedSeconds)
+    println(output)
 }
