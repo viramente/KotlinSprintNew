18bc3913aa334417b0ad821bf2e57a0f38cf8e47
viramente <viramente@gmail.com>
2023-11-25 14:58:29 +0200

    KS-1-3 space flight, receiving Strings

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -2,11 +2,16 @@ package lesson1
 
 fun main() {
 
-    val year = 1969
-    var hour = 9
-    var minute = 7
-    hour = 10
-    minute = 55
+    val year = "1969"
+    var hour = "09"
+    var minute = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")
