f619ff703c58106ae0d86ff0aeb4c28aee1dbc88
viramente <viramente@gmail.com>
2023-11-25 13:16:05 +0200

    KS-3-2 personal data register

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package lesson3
+
+fun main() {
+
+    var age = 20
+    val firstName = "Татьяна"
+    var lastName = "Андреева"
+    val fathersName = "Сергеевна"
+
+    println("$lastName $firstName $fathersName, $age")
+
+    age = 22
+    lastName = "Сидорова"
+
+    println("$lastName $firstName $fathersName, $age")
+
+}
