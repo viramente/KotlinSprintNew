e1283d87b537524ed20c5a9ee6332cbd21d7a0ae
viramente <viramente@gmail.com>
2023-11-28 13:53:28 +0200

    KS-4-2 cargo, 'Average' category, without magic number, with const val

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -5,10 +5,14 @@ fun main() {
     var weight = 20
     var volume = 80
 
-    println ("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= 35) && (weight <= 100) && (volume < 100)}")
+    println ("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= REQUIRED_MIN_WEIGHT_CLOSED) && (weight <= REQUIRED_MAX_WEIGHT_CLOSED) && (volume < REQUIRED_MAX_VOLUME_OPEN)}")
 
     weight = 50
     volume = 100
 
-    println ("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= 35) && (weight <= 100) && (volume < 100)}")
+    println ("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= REQUIRED_MIN_WEIGHT_CLOSED) && (weight <= REQUIRED_MAX_WEIGHT_CLOSED) && (volume < REQUIRED_MAX_VOLUME_OPEN)}")
 }
+
+const val REQUIRED_MIN_WEIGHT_CLOSED = 35
+const val REQUIRED_MAX_WEIGHT_CLOSED = 100
+const val REQUIRED_MAX_VOLUME_OPEN = 100
