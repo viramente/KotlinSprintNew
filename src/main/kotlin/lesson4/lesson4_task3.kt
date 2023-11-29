803355c1f6b3f1baf87bb9c956225caee9985e37
viramente <viramente@gmail.com>
2023-11-29 20:51:12 +0200

    KS-4-3 conditions for growth of legumes, with short code string

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -7,7 +7,10 @@ fun main() {
     val currentAirHumidityInPercents = 20
     val currentSeason = "winter"
 
-    val likelyConditions = (currentSun == IS_SUNNY) && (currentAwningState == IS_AWNING_OPEN) && (currentAirHumidityInPercents == REQUIRED_AIR_HUMIDITY_IN_PERCENTS) && (currentSeason != BAD_SEASON)
+    val likelyConditions = (currentSun == IS_SUNNY) &&
+            (currentAwningState == IS_AWNING_OPEN) &&
+            (currentAirHumidityInPercents == REQUIRED_AIR_HUMIDITY_IN_PERCENTS) &&
+            (currentSeason != BAD_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $likelyConditions")
 
