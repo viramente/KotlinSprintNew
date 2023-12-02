9d9991800beef4d6d097efe9b503fd8ecfe2acef
viramente <viramente@gmail.com>
2023-12-02 15:42:57 +0200

    KS-6-5 authorization with check random numbers sum

---
+++ src/main/kotlin/lesson6/lesson6_task5.kt
@@ -0,0 +1,36 @@
+package lesson6
+
+fun main() {
+
+    var counter = ATTEMPTS_AMOUNT
+
+    while (counter != 0) {
+        println(REQUEST_MESSAGE)
+
+        val firstNumber = (0..9).random()
+        val secondNumber = (0..9).random()
+
+        println("Сколько будет $firstNumber + $secondNumber?")
+
+        val usersAnswer = readln().toInt()
+
+        if (usersAnswer == (firstNumber + secondNumber)) {
+            println(SUCCESSFUL_MESSAGE)
+            break
+        }
+
+        counter--
+
+        if (counter == 0) {
+            println(UNSUCCESSFUL_MESSAGE)
+            break
+        }
+
+        println("Неверно. Осталось попыток: $counter.")
+    }
+}
+
+const val ATTEMPTS_AMOUNT = 3
+const val REQUEST_MESSAGE = "Докажи, что ты не бот."
+const val SUCCESSFUL_MESSAGE = "Добро пожаловать!"
+const val UNSUCCESSFUL_MESSAGE = "Доступ запрещен."
