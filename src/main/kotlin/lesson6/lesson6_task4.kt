bdbd0dbbd7922ffa2330521ca95df2c827d91588
viramente <viramente@gmail.com>
2023-12-02 22:37:39 +0200

    KS-6-4 without unnecessary texts constants and special constant for the amount of the attempts

--- src/main/kotlin/lesson6/lesson6_task4.kt
+++ src/main/kotlin/lesson6/lesson6_task4.kt
@@ -4,28 +4,23 @@ fun main() {
 
     val determinedNumber = (0..9).random()
 
-    println(RULES_MESSAGE)
+    println("Вам нужно угадать число от 0 до 9 за пять попыток.")
 
-    var counter = ATTEMPTS_NUMBER
+    var attemptsCounter = 5
 
-    while (counter > 0) {
+    while (attemptsCounter > 0) {
 
-        println(REQUEST_MESSAGE)
+        println("Введите число:")
         val usersGuessNumber = readln().toInt()
-        counter--
+        attemptsCounter--
 
         if (usersGuessNumber == determinedNumber) {
-            counter = 0
-            println(SUCCESSFUL_MESSAGE)
-        } else if (counter == 0) {
+            attemptsCounter = 0
+            println("Вы угадали! Это была великолепная игра!")
+        } else if (attemptsCounter == 0) {
             println("К сожалению, вам не удалось угадать число. Было загадано число $determinedNumber.")
         } else {
-            println("К сожалению, число неверное. Осталось попыток: $counter.")
+            println("К сожалению, число неверное. Осталось попыток: $attemptsCounter.")
         }
     }
 }
-
-const val RULES_MESSAGE = "Вам нужно угадать число от 0 до 9 за пять попыток."
-const val SUCCESSFUL_MESSAGE = "Вы угадали! Это была великолепная игра!"
-const val REQUEST_MESSAGE = "Введите число:"
-const val ATTEMPTS_NUMBER = 5
