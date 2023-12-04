d6f66cfb307a343f4a7349aad5ca5db64d4b2117
viramente <viramente@gmail.com>
2023-12-04 23:10:56 +0200

    KS-8-5 creating an array of ingredients, -- initializing while creating

--- src/main/kotlin/lesson8/lesson8_task5.kt
+++ src/main/kotlin/lesson8/lesson8_task5.kt
@@ -3,14 +3,9 @@ package lesson8
 fun main() {
 
     println("Сколько ингредиентов будет в списке?")
-
-    val ingredientsAmount = readln().toInt()
-
-    val ingredients = Array(ingredientsAmount) { "" }
-
-    for (index in 0..<ingredients.size) {
-        println("Введите ингредиент ${index + 1}:")
-        ingredients[index] = readln()
+    val ingredients = Array(readln().toInt()) { it ->
+        println("Введите ингредиент ${it + 1}:")
+        readln()
     }
 
 }
