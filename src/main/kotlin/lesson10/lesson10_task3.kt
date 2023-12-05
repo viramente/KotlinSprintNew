3792859f33685b7292d6948aa6593ee6c1ac4ed7
viramente <viramente@gmail.com>
2023-12-05 21:52:09 +0200

    KS-10-3 password generation using pairs "number + special symbol"

---
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -0,0 +1,31 @@
+package lesson10
+
+fun main() {
+
+    println("Укажите длину пароля:")
+    val passwordLength = readln().toInt()
+
+    println(createPassword(passwordLength))
+
+}
+
+fun getRandomNumber() = (0..9).random().digitToChar()
+fun getRandomSpecialSymbol() = "!\"#\$%&'()*+,-./ ".random()
+
+
+fun createPassword(passwordLength: Int): String {
+
+    val password = mutableListOf<Char>()
+
+    for (i in 0..<passwordLength / 2) {
+        password.add(getRandomNumber())
+        password.add(getRandomSpecialSymbol())
+    }
+
+    return (if (passwordLength % 2 == 0) {
+        password.joinToString("")
+    } else {
+        (password + getRandomNumber()).joinToString("")
+    }
+            )
+}
