c0a804e8840fe1b8a9b1e7bbcfd72b28dcd52b40
viramente <viramente@gmail.com>
2023-12-05 18:50:41 +0200

    KS-6-4 game guess the number, with "return" and text moving out of cycle "while"

--- src/main/kotlin/lesson6/lesson6_task4.kt
+++ src/main/kotlin/lesson6/lesson6_task4.kt
@@ -4,23 +4,21 @@ fun main() {
 
     val determinedNumber = (0..9).random()
 
-    println("Вам нужно угадать число от 0 до 9 за пять попыток.")
-
     var attemptsCounter = 5
 
-    while (attemptsCounter > 0) {
+    println("Вам нужно угадать число от 0 до 9 за $attemptsCounter попыток.")
 
+    while (attemptsCounter-- > 0) {
         println("Введите число:")
         val usersGuessNumber = readln().toInt()
-        attemptsCounter--
 
         if (usersGuessNumber == determinedNumber) {
-            attemptsCounter = 0
             println("Вы угадали! Это была великолепная игра!")
-        } else if (attemptsCounter == 0) {
-            println("К сожалению, вам не удалось угадать число. Было загадано число $determinedNumber.")
-        } else {
-            println("К сожалению, число неверное. Осталось попыток: $attemptsCounter.")
+            return
         }
+
+        println("Число неверное. Осталось попыток: $attemptsCounter.")
     }
+
+    println("К сожалению, вам не удалось угадать число. Было загадано число $determinedNumber.")
 }
