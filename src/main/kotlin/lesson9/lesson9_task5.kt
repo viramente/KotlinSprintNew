93f48cd86510dc64de68dc43a3a29e7a7e0d9196
viramente <viramente@gmail.com>
2023-12-06 18:36:51 +0200

    KS-9-5 reformatting list of ingredients to string, with input cycle and smart "joinToString"

--- src/main/kotlin/lesson9/lesson9_task5.kt
+++ src/main/kotlin/lesson9/lesson9_task5.kt
@@ -2,37 +2,14 @@ package lesson9
 
 fun main() {
 
-    println("Введите первый ингредиент:")
-    val firstIngredient = readln()
-
-    println("Введите второй ингредиент:")
-    val secondIngredient = readln()
-
-    println("Введите третий ингредиент:")
-    val thirdIngredient = readln()
-
-    println("Введите четвёртый ингредиент:")
-    val fourthIngredient = readln()
-
-    println("Введите пятый ингредиент:")
-    val fifthIngredient = readln()
-
-    val setOfIngredients = setOf(
-        firstIngredient,
-        secondIngredient,
-        thirdIngredient,
-        fourthIngredient,
-        fifthIngredient,
-    )
+    val setOfIngredients = Array(5) {
+        println("Введите ингредиент ${it + 1}:")
+        readln()
+    }
 
     val orderedListOfIngredients = setOfIngredients.sorted().toMutableList()
 
     orderedListOfIngredients[0] = orderedListOfIngredients[0].replaceFirstChar { it.uppercase() }
 
-    for (i in 0..<orderedListOfIngredients.size - 1) {
-        orderedListOfIngredients[i] = orderedListOfIngredients[i] + ", "
-    }
-
-    println(orderedListOfIngredients.joinToString(""))
-
+    println(orderedListOfIngredients.joinToString(", "))
 }
