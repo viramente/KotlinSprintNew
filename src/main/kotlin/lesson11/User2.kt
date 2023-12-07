ccad8c382fd4ca603d108b6f552a90067a8ae2c4
viramente <viramente@gmail.com>
2023-12-07 16:55:48 +0200

    KS-11-2 class User2, creating and invoking class methods and properties

---
+++ src/main/kotlin/lesson11/User2.kt
@@ -0,0 +1,25 @@
+package lesson11
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null
+) {
+    fun readBio() = println(bio)
+
+    fun addBio(newBio: String) {
+        bio = newBio
+    }
+
+    fun changePassword() {
+        println("Введите действующий пароль:")
+        val newPassword = readln()
+        if (password == newPassword) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль изменён.")
+        }
+    }
+}
