71da10b20794cef39bb2def16e91d4bbfa94e4fd
viramente <viramente@gmail.com>
2023-12-10 01:18:43 +0200

    KS-11-2 class User2, creating and invoking class methods and properties -- combine into one file

--- src/main/kotlin/lesson11/task2/lesson11_task2.kt
+++ src/main/kotlin/lesson11/task2/lesson11_task2.kt
@@ -14,3 +14,27 @@ fun main() {
     user3.readBio()
     println(user3.password)
 }
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null
+) {
+    fun readBio() = println(bio)
+
+    fun addBio(newBio: String) {
+        bio = newBio
+    }
+
+    fun changePassword() {
+        println("Введите действующий пароль:")
+        val newPassword = readln()
+        if (password == newPassword) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль изменён.")
+        }
+    }
+}
