7c97c21a8bb944838ca8716a87930ce9ab658454
viramente <viramente@gmail.com>
2023-12-09 23:54:58 +0200

    KS-12-2 weather for day

---
+++ src/main/kotlin/lesson12/lesson12_task2.kt
@@ -0,0 +1,27 @@
+package lesson12
+
+fun main() {
+
+    val wednesday = DayWeather1(12, 5, "дождь")
+    val thursday = DayWeather1(14, 8, "дождь")
+
+    wednesday.getWeather()
+    thursday.getWeather()
+}
+
+class DayWeather1(
+    private val dayTemperature: Int,
+    private val nightTemperature: Int,
+    private val precipitation: String,
+) {
+    fun getWeather() {
+        println(
+            """
+            Погода на день: 
+            температура днем: $dayTemperature °C,
+            температура ночью: $nightTemperature °C,
+            осадки: $precipitation
+            """.trimIndent()
+        )
+    }
+}
