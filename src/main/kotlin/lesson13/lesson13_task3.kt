a521628c13ef8209df87f1732ced32266a5bf9a8
viramente <viramente@gmail.com>
2023-12-10 22:32:17 +0200

    KS-13-3 telephone directory, printing out not nullable companies' names

--- src/main/kotlin/lesson13/lesson13_task3.kt
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -3,18 +3,18 @@ package lesson13
 fun main() {
 
     val listOfSubscribers = listOf(
-        TelephoneDirectory3("Марина", 89764598539, "Марина Мирабелла"),
-        TelephoneDirectory3("Олег Геннадьевич Митрохин", 89745305594, "Градостроительство"),
-        TelephoneDirectory3("Марис", 89692098500, null),
-        TelephoneDirectory3("Сантехник", 89701598936, "null"),
-        TelephoneDirectory3("Наталья Андреевна", 89904598544, null),
+        TelephoneSubscriber3("Марина", 89764598539, "Марина Мирабелла"),
+        TelephoneSubscriber3("Олег Геннадьевич Митрохин", 89745305594, "Градостроительство"),
+        TelephoneSubscriber3("Марис", 89692098500, null),
+        TelephoneSubscriber3("Сантехник", 89701598936, "null"),
+        TelephoneSubscriber3("Наталья Андреевна", 89904598544, null),
     )
 
     listOfSubscribers.filter { it.company != null }.forEach { println(it.company) }
 
 }
 
-class TelephoneDirectory3 (
+class TelephoneSubscriber3 (
     val name: String,
     val phoneNumber: Long,
     val company: String? = null,
