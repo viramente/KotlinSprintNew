ea5061b3b131e450cede8a1024580760e26fae8d
viramente <viramente@gmail.com>
2023-12-10 22:34:28 +0200

    KS-13-2 telephone directory, nullable company, renaming class

--- src/main/kotlin/lesson13/lesson13_task2.kt
+++ src/main/kotlin/lesson13/lesson13_task2.kt
@@ -2,14 +2,14 @@ package lesson13
 
 fun main() {
 
-    val rostislav = TelephoneDirectory2 ("Ростислав", 89123456789, "Reddit")
-    val jenya = TelephoneDirectory2 ("Женя", 8912880169)
+    val rostislav = TelephoneSubscriber2 ("Ростислав", 89123456789, "Reddit")
+    val jenya = TelephoneSubscriber2 ("Женя", 8912880169)
 
     rostislav.getInfo()
     jenya.getInfo()
 }
 
-class TelephoneDirectory2 (
+class TelephoneSubscriber2 (
     val name: String,
     val phoneNumber: Long,
     val company: String? = null,
