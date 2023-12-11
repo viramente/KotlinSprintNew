0e9af0d69b22b1c2a58b041c6d399fe6cc8753bf
viramente <viramente@gmail.com>
2023-12-11 23:29:45 +0200

    KS-14-3 circles and rectangles, areas and perimeters

---
+++ src/main/kotlin/lesson14/task3/lesson14_task3.kt
@@ -0,0 +1,40 @@
+package lesson14.task3
+
+import kotlin.math.PI
+import kotlin.math.pow
+
+
+fun main() {
+
+    val figures = listOf<Figure>(
+    Circle(10.8, "чёрный"),
+    Circle(18.5, "белый"),
+    Circle(5.4, "чёрный"),
+    Circle(15.0, "белый"),
+    Rectangle(11.0, 12.0, "чёрный"),
+    Rectangle(8.1, 8.0, "чёрный"),
+    Rectangle(12.5, 50.0, "белый"),
+    )
+
+    println("Сумма периметров всех чёрных фигур:\n" +
+            "${figures.filter {it.color == "чёрный"}.sumOf { it.calculatePerimeter() }}")
+
+    println("Сумма площадей всех белых фигур:\n" +
+            "${figures.filter {it.color == "белый"}.sumOf {it.calculateArea()}}")
+}
+
+abstract class Figure() {
+    abstract val color: String
+    abstract fun calculateArea(): Double
+    abstract fun calculatePerimeter(): Double
+}
+
+open class Circle(private val radiusInCm: Double, override val color: String): Figure() {
+    override fun calculateArea() = radiusInCm * 2 * PI
+    override fun calculatePerimeter() = (radiusInCm.pow(2.0) * PI)
+}
+
+class Rectangle(private val height: Double, private val width: Double, override val color: String): Figure() {
+    override fun calculateArea() = height * width
+    override fun calculatePerimeter() = (height + width) * 2
+}
