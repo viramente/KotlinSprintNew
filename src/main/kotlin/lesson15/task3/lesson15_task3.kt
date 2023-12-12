318a96a2dd32c7eb754a0ce3eb9c31ace62374c8
viramente <viramente@gmail.com>
2023-12-12 21:21:43 +0200

    KS-15-3 musical instruments, details searching

--- src/main/kotlin/lesson15/task3/lesson15_task3.kt
+++ src/main/kotlin/lesson15/task3/lesson15_task3.kt
@@ -1,36 +1,36 @@
 package lesson15.task3
-
-fun main() {
-
-    val oboe = Instrument("гобой", 12)
-    val guitar = Instrument("гитара", 23)
-    val strings = Detail("струны", 123, guitar)
-    val pins = Detail("колки", 5, guitar)
-    val stick = Detail("трость", 81, oboe)
-
-    val details = listOf(strings, pins, stick)
-
-    oboe.searchDetails(details)
-}
-
-interface Searchable {
-    fun searchDetails(details: List<Detail>) {
-        println("Выполняется поиск...")
-    }
-}
-
-abstract class Good(
-    val name: String,
-    val quantityInWarehouse: Int,
-)
-
-class Instrument(
-    name: String,
-    quantityInWarehouse: Int
-) : Good(name, quantityInWarehouse), Searchable
-
-class Detail(
-    name: String,
-    quantityInWarehouse: Int,
-    val parentInstrument: Instrument
-) : Good(name, quantityInWarehouse)
+//
+//fun main() {
+//
+//    val oboe = Instrument("гобой", 12)
+//    val guitar = Instrument("гитара", 23)
+//    val strings = Detail("струны", 123, guitar)
+//    val pins = Detail("колки", 5, guitar)
+//    val stick = Detail("трость", 81, oboe)
+//
+//    val details = listOf(strings, pins, stick)
+//
+//    oboe.searchDetails(details)
+//}
+//
+//interface Searchable {
+//    fun searchDetails(details: List<Detail>) {
+//        println("Выполняется поиск...")
+//    }
+//}
+//
+//abstract class Good(
+//    val name: String,
+//    val quantityInWarehouse: Int,
+//)
+//
+//class Instrument(
+//    name: String,
+//    quantityInWarehouse: Int
+//) : Good(name, quantityInWarehouse), Searchable
+//
+//class Detail(
+//    name: String,
+//    quantityInWarehouse: Int,
+//    val parentInstrument: Instrument
+//) : Good(name, quantityInWarehouse)
