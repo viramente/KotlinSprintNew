f7438d9ae644de794d206b5123419112c5fc47cb
viramente <viramente@gmail.com>
2023-12-14 19:03:52 +0200

    KS-14-4 planets and satellites, -- list of satellites in constructor
    # Conflicts:
    #	src/main/kotlin/lesson15/task3/lesson15_task3.kt

--- src/main/kotlin/lesson14/task4/lesson14_task4.kt
+++ src/main/kotlin/lesson14/task4/lesson14_task4.kt
@@ -2,10 +2,13 @@ package lesson14.task4
 
 fun main() {
 
-    val koenigsberg = Planet("Кёнигсберг", true, false)
-    koenigsberg.listOfSatellites = listOf(
-        Satellite("Розенау", false, false),
-        Satellite("Понарт", false, false),
+    val koenigsberg = Planet(
+        "Кёнигсберг",
+        true,
+        false,
+        listOf(Satellite("Розенау", false, false),
+            Satellite("Понарт", false, false),
+        )
     )
 
     println("Планета: ${koenigsberg.name}, " +
@@ -15,8 +18,6 @@ fun main() {
                 .joinToString (", " )}."
     )
 }
-
-
 
 open class CelestialBodies (
     val name: String,
@@ -30,13 +31,9 @@ class Satellite(
     isSuitableForLanding: Boolean,
 ) : CelestialBodies(name, hasAtmosphere, isSuitableForLanding)
 
-
 class Planet(
     name: String,
     hasAtmosphere: Boolean,
     isSuitableForLanding: Boolean,
-) : CelestialBodies(name, hasAtmosphere, isSuitableForLanding) {
-    var listOfSatellites = listOf<Satellite>()
-}
-
-
+    val listOfSatellites: List<Satellite>,
+) : CelestialBodies(name, hasAtmosphere, isSuitableForLanding)
