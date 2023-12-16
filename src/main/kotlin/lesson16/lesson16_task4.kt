a01a30fcbdcbaa30518865b1b3f95c6a9c4f2df4
viramente <viramente@gmail.com>
2023-12-16 23:32:12 +0200

    KS-16-4 class user, prohibited change order's number, ask manager for status change permission -- private status

--- src/main/kotlin/lesson16/lesson16_task4.kt
+++ src/main/kotlin/lesson16/lesson16_task4.kt
@@ -4,12 +4,13 @@ fun main() {
 
     val order = Order(12, "принят в работу")
     order.askManagerToChangeStatus("перемещен в цех упаковки")
+    order.getStatus()
 
 }
 
 class Order(val ordersNumber: Int, _status: String) {
 
-    var status = _status
+    private var status = _status
 
     private fun changeOrdersStatus(newStatus: String)  {
         status = newStatus
@@ -17,4 +18,6 @@ class Order(val ordersNumber: Int, _status: String) {
 
     fun askManagerToChangeStatus(newStatus: String) =
         changeOrdersStatus(newStatus)
+
+    fun getStatus() = println("Заказ $ordersNumber $status.")
 }
