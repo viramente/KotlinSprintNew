bde056917d0fd4fbe86a8a412a450bd3f41f380d
viramente <viramente@gmail.com>
2023-12-23 00:01:54 +0200

    KS-19-3 spaceship with TODOs

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,12 @@
+package lesson19
+
+abstract class SpaceshipL19T3() {
+
+    fun takeOff() = println("")
+    //TODO: тут надо дописать логику
+
+    fun land(): Nothing = TODO()
+
+    abstract fun shootBackAtAnAsteroid()
+
+}
