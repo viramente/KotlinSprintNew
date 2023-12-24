ae2e87f3491a140d46754f6779a929ef5191e521
viramente <viramente@gmail.com>
2023-12-24 15:06:06 +0200

    KS-20-3 renaming player class name

--- src/main/kotlin/lesson20/lesson20_task3.kt
+++ src/main/kotlin/lesson20/lesson20_task3.kt
@@ -2,10 +2,10 @@ package lesson20
 
 fun main() {
 
-    val agneshka = PlayerL20T2("Агнешка", false)
-    val nargiz = PlayerL20T2("Наргиз", true)
+    val agneshka = PlayerL20T3("Агнешка", false)
+    val nargiz = PlayerL20T3("Наргиз", true)
 
-    val openDoor = { player: PlayerL20T2 ->
+    val openDoor = { player: PlayerL20T3 ->
         println (if (player.hasKey) "${player.name}, вы открыли дверь."
         else ("${player.name}, дверь заперта."))
     }
@@ -15,4 +15,4 @@ fun main() {
 
 }
 
-class PlayerL20T2(val name: String, val hasKey: Boolean)
+class PlayerL20T3(val name: String, val hasKey: Boolean)
